22c5b8d324a3e418b5a3fe8d8114ebf1b59bfbfb
atsumo <bachisuta@gmail.com>
2015-03-19 09:46:12 +0900

    update icons

--- markdown/src/js/index.jsx
+++ markdown/src/js/index.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
 import App from './components/App.jsx';
+import {gui} from 'nw.gui';
+
+if (process.platform === "darwin") {
+  var mb = new gui.Menu({type: 'menubar'});
+  mb.createMacBuiltin('RoboPaint', {
+    hideEdit: false,
+  });
+  gui.Window.get().menu = mb;
+}
 
 React.render(<App />, document.getElementById('app'));
