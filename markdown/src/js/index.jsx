9cb2e46a168f0ce0264a18e6f689e3f1b08ca3a5
atsumo <bachisuta@gmail.com>
2015-03-19 09:48:28 +0900

    update package.json

--- markdown/src/js/index.jsx
+++ markdown/src/js/index.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
 import App from './components/App.jsx';
-import {gui} from 'nw.gui';
-
-if (process.platform === "darwin") {
-  var mb = new gui.Menu({type: 'menubar'});
-  mb.createMacBuiltin('RoboPaint', {
-    hideEdit: false,
-  });
-  gui.Window.get().menu = mb;
-}
 
 var gui = window.require('nw.gui');
 
